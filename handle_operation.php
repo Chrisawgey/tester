9f61306207d98d5ce610bdc274f3b89dd1b72939
Chrisawgey <124189207+Chrisawgey@users.noreply.github.com>
2023-09-29 23:10:14 -0400

    popups

--- handle_operation.php
+++ handle_operation.php
@@ -7,12 +7,13 @@
 <body>
 <?php
 //you were using the variables '$operation', '$num1' and '$num2' before they were defined
-// Retrieve and cast the data by using $_REQUEST or $_POST and name of the textboxes
+
 //aded line 12-14
 //all i did was make them a funct
 $num1 = isset($_POST['number1']) ? (int)$_POST['number1'] : 0;
 $num2 = isset($_POST['number2']) ? (int)$_POST['number2'] : 0;
 $operation = isset($_POST['operation']) ? $_POST['operation'] : '';
+//isset means checks whether a variable has been set with a value, pretty much checking if it exists 
 
 // Validation of input data, check empty
 //changed from POST to $num1
@@ -61,44 +62,3 @@ if (empty($num1) || empty($num2)) {
 
 </body>
 </html>
-
-/*
-PHP if....else...elseif statement
-if statement - executes some code if one condition is true
-if...else statement - executes some code if a condition is true and another code if that condition is false
-if...elseif....else statement - executes different codes for more than two conditions
-switch statement - selects one of many blocks of code to be executed
-https://www.w3schools.com/php/php_if_else.asp
-*/
-//
-//you code starts from here
-//
-//validation of the input using is_numeric() function, data type checking
-
-/*if($operation == "add"){
-    $result = $num1 + $num2;
-}elseif ($operation == "subtract"){
-    $result = $num1 - $num2;
-}elseif ($operation == "multiply") {
-    $result = $num1 * $num2;
-}elseif ($operation == "divide") {
-    if($num2 != 0) {
-        $result = $num1 / $num2;
-    }else{
-        echo "Cannot divide by 0. Please try again."
-    }
-}elseif ($operation == "modulus"){
-    $result = $num1 % $num2;
-}*/
-
-
-
-/*if ($operation == "add") $result = $num1+$num2;
-if ($operation == "subtract") $result = $num1-$num2;
-if ($operation == "multiply") $result = $num1*$num2;
-if ($operation == "divide") $result = $num1/$num2;
-if ($operation == "modulus") $result = $num1%$num2;*/
-
-
-/*// Print the submitted information:
-echo "" The result is $result";*/
